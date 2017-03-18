cf3968320e29bafd494f65e3d48ef8ca4b3f4cd8
Will <william.dunlop.1993@gmail.com>
2017-03-18 12:26:57 +1100

    minor changes

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Will's Wonderful Base Redux Proj</div>
+      <div className="row top-buffer">
+        <div className="col-md-6 col-md-offset-3">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3 className="panel-title">Will's Wonderful Base Redux Project</h3>
+            </div>
+            <div className="panel-body">
+              Use this as a starting point
+            </div>
+          </div>
+        </div>
+      </div>
+);
     );
   }
 }
